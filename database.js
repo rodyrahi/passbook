ee1bc01087fb66206c0f014a7aaeec6734550e84
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-08-04 12:24:29 +0530

    test

--- database.js
+++ database.js
@@ -1,12 +1,40 @@
-var Host  = "165.232.151.6" 
+const Host = process.platform ===  "127.0.0.1" ? "win32": "165.232.151.6";
 var mysql = require("mysql");
-var isWin = process.platform === "win32";
+// var isWin = process.platform === "win32";
 
-if (!isWin) {
-  Host = "127.0.0.1"
+// if (!isWin) {
+//   Host = "127.0.0.1"
   
+// }
+// console.log(Host);
+
+
+
+
+
+const { exec } = require("child_process");
+const cron = require("node-cron");
+
+// Function to run the mysqldump command and save the backup to all.sql
+function runMysqldump() {
+  const backupFileName = "all.sql";
+  const mysqldumpCommand = `mysqldump -u root -p --all-databases > ${backupFileName}`;
+
+  exec(mysqldumpCommand, (error, stdout, stderr) => {
+    if (error) {
+      console.error("Error running mysqldump:", error);
+      return;
+    }
+    console.log(`Backup saved to ${backupFileName}`);
+  });
 }
-console.log(Host);
+
+// Schedule the mysqldump to run every 24 hours
+cron.schedule("*/15 * * * * *", () => {
+  console.log("Starting database backup...");
+  runMysqldump();
+});
+
 
 var connection = mysql.createConnection({
   host: Host,
